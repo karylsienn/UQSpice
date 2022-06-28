49653a614ee093b6db028c93fefe8756ba45b076
Mohammed-Ibrahim99 <moaik19@gmail.com>
2022-06-28 15:38:01 +0100

    Update main.py
    Implemented resizing of schematic

--- GUI/main.py
+++ GUI/main.py
@@ -9,6 +9,7 @@ from pandas import DataFrame
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPDF, renderPM
 from PIL import Image, ImageTk
+
 # ----------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 # -------------------------------------------------GUI Program beta v0.1------------------------------------------------
@@ -16,10 +17,33 @@ from PIL import Image, ImageTk
 # ----------------------------------------------------------------------------------------------------------------------
 # Opening a file using tkinter
 
+circuit_components = []
+
+
+# a subclass of Canvas for dealing with resizing of windows
+class ResizingCanvas(Canvas):
+    def __init__(self, parent, **kwargs):
+        Canvas.__init__(self, parent, **kwargs)
+        self.bind("<Configure>", self.on_resize)
+        self.height = self.winfo_reqheight()
+        self.width = self.winfo_reqwidth()
+
+    def on_resize(self, event):
+        # determine the ratio of old width/height to new width/height
+        width_scale = float(event.width) / self.width
+        height_scale = float(event.height) / self.height
+        self.width = event.width
+        self.height = event.height
+        # resize the canvas
+        self.config(width=self.width, height=self.height)
+        # rescale all the objects tagged with the "all" tag
+        self.scale("all", 0, 0, width_scale, height_scale)
+
+
 # create the root window
 root = tk.Tk()
 root.title('EMC Analysis')
-root.geometry('1080x720')
+root.geometry('1080x720+250+200')
 
 tabControl = ttk.Notebook(root)
 
@@ -29,7 +53,7 @@ graphs = ttk.Frame(tabControl)
 tabControl.add(schematic_params, text='Schematic and entering parameters')
 tabControl.add(graphs, text='Graphs')
 
-canvas = Canvas(schematic_params, width=1080, height=720)
+canvas = ResizingCanvas(schematic_params, width=700, height=500, highlightthickness=0)
 
 all_component_parameters = []
 name_label = Label(canvas, text='')
@@ -109,8 +133,8 @@ def beta_distribution_params(component_distribution,
 # ----------------------------------------------------------------------------------------------------------------------
 def sketch_graphs(data):
     data = {'Country': ['US', 'CA', 'GER', 'UK', 'FR'],
-             'GDP_Per_Capita': [45000, 42000, 52000, 49000, 47000]
-             }
+            'GDP_Per_Capita': [45000, 42000, 52000, 49000, 47000]
+            }
     data_frame_plot = DataFrame(data, columns=['Country', 'GDP_Per_Capita'])
     figure = plt.Figure(figsize=(10, 6), dpi=100)
     ax = figure.add_subplot(111)
@@ -322,10 +346,10 @@ def save_entered_parameters(entering_parameters_window,
 
     if len(all_component_parameters) == 0:
         all_component_parameters.append({component_name:
-                                        {'distribution': component_distribution,
-                                         'parameters': {component_param1_label: component_param1,
-                                                        component_param2_label: component_param2}
-                                         }
+                                             {'distribution': component_distribution,
+                                              'parameters': {component_param1_label: component_param1,
+                                                             component_param2_label: component_param2}
+                                              }
                                          }
                                         )
 
@@ -336,13 +360,13 @@ def save_entered_parameters(entering_parameters_window,
             # If the last entered component is similar to the previously entered one then,
             # replace the old parameters with the new ones
             all_component_parameters[parameters] = ({component_name:
-                                                    {'distribution': component_distribution,
-                                                     'parameters': {component_param1_label: component_param1,
-                                                                    component_param2_label: component_param2}
-                                                     }
+                                                         {'distribution': component_distribution,
+                                                          'parameters': {component_param1_label: component_param1,
+                                                                         component_param2_label: component_param2}
+                                                          }
                                                      }
 
-                                                    )
+            )
             # Ensures no appending takes place
             appending_flag = 0
             break
@@ -353,13 +377,13 @@ def save_entered_parameters(entering_parameters_window,
 
     if appending_flag == 1:
         all_component_parameters.append({component_name:
-                                        {'distribution': component_distribution,
-                                         'parameters': {component_param1_label: component_param1,
-                                                        component_param2_label: component_param2}
-                                         }
+                                             {'distribution': component_distribution,
+                                              'parameters': {component_param1_label: component_param1,
+                                                             component_param2_label: component_param2}
+                                              }
                                          }
 
-        )
+                                        )
         appending_flag = 0
 
     print(all_component_parameters)
@@ -368,9 +392,9 @@ def save_entered_parameters(entering_parameters_window,
     full_name_labels[index].config(text='')
     full_name_labels[index] = Label(canvas,
                                     text=component_name +
-                                    '\nDistribution: ' + component_distribution +
-                                    '\n' + component_param1_label + '=' + component_param1 +
-                                    '\n' + component_param2_label + '=' + component_param2)
+                                         '\nDistribution: ' + component_distribution +
+                                         '\n' + component_param1_label + '=' + component_param1 +
+                                         '\n' + component_param2_label + '=' + component_param2)
 
     name_label_window = canvas.create_window(800,
                                              (100 * index) + 100, anchor=E,
@@ -388,7 +412,6 @@ def save_all_entered_parameters(component_name,
                                 component_param1_array,
                                 component_param2_array,
                                 full_name_labels):
-
     global name_label
     global all_component_parameters
     all_component_parameters.clear()
@@ -399,17 +422,18 @@ def save_all_entered_parameters(component_name,
         full_name_labels[circuit_component].config(text='')
 
         # Storing the name label of all parameters
-        full_name_labels[circuit_component] =\
+        full_name_labels[circuit_component] = \
             Label(canvas,
                   text=component_name[circuit_component] +
 
-                  '\nDistribution: ' + component_distribution_array[circuit_component].get('1.0', END).strip('\n') +
+                       '\nDistribution: ' + component_distribution_array[circuit_component].get('1.0', END).strip(
+                      '\n') +
 
-                  '\n' + component_param1_label_array[circuit_component]['text']
-                  + '=' + component_param1_array[circuit_component].get('1.0', END).strip('\n') +
+                       '\n' + component_param1_label_array[circuit_component]['text']
+                       + '=' + component_param1_array[circuit_component].get('1.0', END).strip('\n') +
 
-                  '\n' + component_param2_label_array[circuit_component]['text']
-                  + '=' + component_param2_array[circuit_component].get('1.0', END).strip('\n'))
+                       '\n' + component_param2_label_array[circuit_component]['text']
+                       + '=' + component_param2_array[circuit_component].get('1.0', END).strip('\n'))
 
         # Placing the name label of all parameters on the root window
         name_label_window = canvas.create_window(800, (100 * circuit_component) + 100, anchor=E,
@@ -419,15 +443,15 @@ def save_all_entered_parameters(component_name,
         # Storing all components with their parameters in a dictionary
         all_component_parameters.append(
             {component_name[circuit_component]:
-             {'distribution': component_distribution_array[circuit_component].get('1.0', END).strip('\n'),
-              'parameters': {component_param1_label_array[circuit_component]['text']:
-                             component_param1_array[circuit_component].get('1.0', END).strip('\n'),
+                 {'distribution': component_distribution_array[circuit_component].get('1.0', END).strip('\n'),
+                  'parameters': {component_param1_label_array[circuit_component]['text']:
+                                     component_param1_array[circuit_component].get('1.0', END).strip('\n'),
 
-                             component_param2_label_array[circuit_component]['text']:
-                             component_param2_array[circuit_component].get('1.0', END).strip('\n')}
-              }
+                                 component_param2_label_array[circuit_component]['text']:
+                                     component_param2_array[circuit_component].get('1.0', END).strip('\n')}
+                  }
              }
-              )
+        )
 
     print(all_component_parameters)
 
@@ -438,9 +462,19 @@ def save_all_entered_parameters(component_name,
 # Entering Component Parameters for all elements
 def component_parameters():
     global all_component_parameters
-    all_component_parameters.clear()
-
-    open_new_window(circuit_components, 0)
+    global circuit_components
+    print(type(all_component_parameters))
+    if len(circuit_components) != 0:
+        open_new_window(circuit_components, 0)
+    else:
+        error_for_not_entering_schematic = \
+            canvas.create_window(500, 300,
+                                 window=Label(canvas,
+                                              text="Please select a schematic first",
+                                              width=40,
+                                              font=("Courier", 20),
+                                              height=40),
+                                 tags='Error if schematic not entered')
 
 
 # Replacing the oval function of tkinter with a simpler function for circles
@@ -487,7 +521,6 @@ def get_file_path():
     # size = img.size
     # canvas.create_image(20, 20, anchor=NW, image=pimg, tags='schematic')
 
-    
     # TODO: Guess the encoding
     fpath = file_path[0]
     with open(fpath, 'rb') as ltspiceascfile:
@@ -504,9 +537,7 @@ def get_file_path():
         schematic = ltspiceascfile.readlines()
         # TODO: read the remainder with readlines(encoding=encoding)
     ltspiceascfile.close()
-    # ltspiceascfile = open(file_path[0], "r")
-    # schematic = ltspiceascfile.readlines()
-    # ltspiceascfile.close()
+
     sketch_schematic_asc(schematic)
 
     # Display file path at the bottom of the window
@@ -520,6 +551,7 @@ def get_file_path():
 def sketch_schematic_asc(schematic):
     # Remove all previous schematic drawings
     canvas.delete('schematic')
+    canvas.delete('all')
     # Clear all previous labels in root window of component parameters
     global name_label
     # Clear all previous component parameters
@@ -639,7 +671,7 @@ def sketch_schematic_asc(schematic):
                               modified_ground_flags[flag_coordinates], modified_ground_flags[flag_coordinates + 1] + 25,
                               tags='schematic')
 
-    enter_parameters_window = canvas.create_window(150, 650, anchor=NW, window=enter_parameters_button)
+    # enter_parameters_window = canvas.create_window(150, 650, anchor=NW, window=enter_parameters_button)
 
     # --------------------------------------------Binding events--------------------------------------------------------
 
@@ -668,7 +700,9 @@ value = 0
 sketch_graphs(value)
 # open file button, tab control and canvas location in root window
 openfile_button_window = canvas.create_window(20, 650, anchor=NW, window=openfile_button)
+enter_parameters_button.pack(padx=0, pady=10, side=BOTTOM)
+openfile_button.pack(padx=0, pady=2, side=BOTTOM)
 tabControl.pack(expand=True, fill=BOTH)
-canvas.pack(expand=True)
+canvas.pack(fill=BOTH, expand=True)
 # run the application
 root.mainloop()
