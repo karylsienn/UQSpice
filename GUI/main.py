7210069fea1c239301a30b102f22f91082ef7054
Mohammed-Ibrahim99 <moaik19@gmail.com>
2022-07-04 17:00:54 +0100

    Made graph more interactive

--- GUI/main.py
+++ GUI/main.py
@@ -5,8 +5,9 @@ import customtkinter
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from pandas import DataFrame
+import mplcursors
 # import plotly.graph_objects as go
-# import numpy as np
+import numpy as np; np.random.seed(1)
 # from matplotlib.figure import Figure
 # from matplotlib.widgets import Slider
 # from svglib.svglib import svg2rlg
@@ -395,17 +396,55 @@ def sketch_graphs(data):
     # canvas.mpl_connect('scroll_event', tracker.onscroll)
     # canvas.mpl_connect('button_release_event', tracker.contrast)  # add this for contrast change
 
-    data = {'Country': ['US', 'CA', 'GER', 'UK', 'FR'],
-            'GDP_Per_Capita': [45000, 42000, 52000, 49000, 47000]
-            }
-    data_frame_plot = DataFrame(data, columns=['Country', 'GDP_Per_Capita'])
+    # data = {'Country': ['US', 'CA', 'GER', 'UK', 'FR'],
+    #         'GDP_Per_Capita': [45000, 42000, 52000, 49000, 47000]
+    #         }
+    # data_frame_plot = DataFrame(data, columns=['Country', 'GDP_Per_Capita'])
     figure = plt.Figure(figsize=(10, 6), dpi=100)
     ax = figure.add_subplot(111)
-    chart_type = FigureCanvasTkAgg(figure, graphs)
-    chart_type.get_tk_widget().pack(pady=0, fill=BOTH)
-    data_frame_plot = data_frame_plot[['Country', 'GDP_Per_Capita']].groupby('Country').sum()
-    data_frame_plot.plot(kind='line', legend=True, ax=ax)
+    x = np.sort(np.random.rand(15))
+    y = np.sort(np.random.rand(15))
+    names = np.array(list("ABCDEFGHIJKLMNO"))
+
+    line, = ax.plot(x, y)
+
+    chart_type= FigureCanvasTkAgg(figure,master=graphs)
+    chart_type.get_tk_widget().pack(side='top', fill='both', expand=1)
+    # chart_type = FigureCanvasTkAgg(figure, graphs)
+    # chart_type.get_tk_widget().pack(pady=0, fill=BOTH)
+    # data_frame_plot = data_frame_plot[['Country', 'GDP_Per_Capita']].groupby('Country').sum()
+    # data_frame_plot.plot(kind='line', legend=True, ax=ax)
     ax.set_title('Example Plot')
+    ax.grid('on')
+    names = np.array(list("ABCDEFGHIJKLMNO"))
+    annot = ax.annotate("", xy=(0, 0), xytext=(-20, 20), textcoords="offset points",
+                        bbox=dict(boxstyle="round", fc="w"),
+                        arrowprops=dict(arrowstyle="->"))
+    annot.set_visible(False)
+
+    def update_annot(ind):
+        x, y = line.get_data()
+        annot.xy = (x[ind["ind"][0]], y[ind["ind"][0]])
+        text = "{}, {}".format(" ".join(list(map(str, ind["ind"]))),
+                               " ".join([names[n] for n in ind["ind"]]))
+        annot.set_text(text)
+        annot.get_bbox_patch().set_alpha(0.4)
+
+    def hover(event):
+        vis = annot.get_visible()
+        if event.inaxes == ax:
+            cont, ind = line.contains(event)
+            if cont:
+                update_annot(ind)
+                annot.set_visible(True)
+                chart_type.draw_idle()
+            else:
+                if vis:
+                    annot.set_visible(False)
+                    chart_type.draw_idle()
+
+    chart_type.mpl_connect("motion_notify_event", hover)
+
     toolbar = NavigationToolbar2Tk(chart_type, graphs, pack_toolbar=False)
     toolbar.update()
     toolbar.pack(side=BOTTOM, fill=BOTH)
