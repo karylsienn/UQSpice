30815852de24878be064de2042dc6fac3e906178
Mohammed-Ibrahim99 <moaik19@gmail.com>
2022-07-08 10:48:51 +0100

    Added zoom function to resizing canvas class Separated into more file tkinter_modification.py for modifying any tkinter elements and gui_events.py for functions of buttons and other events which occur in GUI

--- GUI/main.py
+++ GUI/main.py
@@ -1,7 +1,12 @@
 ﻿# GUI controller containing all functions
 import controller as control
 
+
+def main():
+    control.schematic_analysis.mainloop()
+
+
 # Run main GUI application
 if __name__ == '__main__':
     # Starting the GUI program
-    control.schematic_analysis.mainloop()
+    main()
